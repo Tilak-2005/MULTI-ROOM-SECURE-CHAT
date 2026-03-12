f677706c70bff37d30d618d63c2139425dad202c
TILAK GIRIDHAR KUMTA <tilakkumta30112005@gmail.com>
2026-03-12 08:03:28 +0530

    Add files via upload

---
+++ Client/client.py
@@ -0,0 +1,82 @@
+import socket
+import threading
+import os
+
+HOST = "127.0.0.1"
+PORT = 5000
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((HOST, PORT))
+
+
+def receive():
+
+    while True:
+        try:
+            msg = client.recv(4096)
+
+            if not msg:
+                break
+
+            msg = msg.decode()
+
+            if msg.startswith("FILE"):
+                parts = msg.split("|")
+                filename = parts[1]
+                size = int(parts[2])
+
+                data = client.recv(size)
+
+                with open("files/" + filename, "wb") as f:
+                    f.write(data)
+
+                print(f"\nFile received: {filename}")
+
+            else:
+                print(msg)
+
+        except:
+            break
+
+
+def send():
+
+    while True:
+
+        msg = input()
+
+        if msg.startswith("/join"):
+            room = msg.split()[1]
+            client.send(f"JOIN|{room}".encode())
+
+        elif msg.startswith("/leave"):
+            room = msg.split()[1]
+            client.send(f"LEAVE|{room}".encode())
+
+        elif msg.startswith("/pm"):
+            parts = msg.split(" ", 2)
+            user = parts[1]
+            message = parts[2]
+            client.send(f"PRIVATE|{user}|{message}".encode())
+
+        elif msg.startswith("/sendfile"):
+
+            parts = msg.split()
+            room = parts[1]
+            path = parts[2]
+
+            filename = os.path.basename(path)
+            size = os.path.getsize(path)
+
+            client.send(f"FILE|{room}|{filename}|{size}".encode())
+
+            with open(path, "rb") as f:
+                client.send(f.read())
+
+        else:
+            room = "room1"
+            client.send(f"MSG|{room}|{msg}".encode())
+
+
+threading.Thread(target=receive).start()
+send()
