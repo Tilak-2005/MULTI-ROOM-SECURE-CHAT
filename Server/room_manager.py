dec80f37df4865a4cfaf445a5ef6d8ee6e74c866
TILAK GIRIDHAR KUMTA <tilakkumta30112005@gmail.com>
2026-03-13 12:55:01 +0530

    Prevent duplicate clients in join_room method
    Prevent adding duplicate clients to the same room.

--- Server/room_manager.py
+++ Server/room_manager.py
@@ -8,19 +8,27 @@ class RoomManager:
         self.sequence = {}
 
     def join_room(self, room, client):
+
         with self.lock:
+
             if room not in self.rooms:
                 self.rooms[room] = []
                 self.sequence[room] = 0
-            self.rooms[room].append(client)
+
+            if client not in self.rooms[room]:
+                self.rooms[room].append(client)
 
     def leave_room(self, room, client):
+
         with self.lock:
+
             if room in self.rooms and client in self.rooms[room]:
                 self.rooms[room].remove(client)
 
     def broadcast(self, room, message, sender=None):
+
         with self.lock:
+
             if room not in self.rooms:
                 return
 
